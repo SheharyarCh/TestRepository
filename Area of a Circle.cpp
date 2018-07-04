b958be9a43190da3bc7e6836dd11c04636fb9baa
SheharyarCh <39981678+SheharyarCh@users.noreply.github.com>
2018-07-04 17:31:19 +0500

    Program - Area of the Circle

---
+++ Area of a Circle.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+	int r;
+	float a;
+	cout<<"Enter the Radius of circle: ";
+	cin>>r;
+	
+	a = 3.14 * r * r;
+	cout<<"The Area of Circle = "<<a;
+	
+	
+	getch();
+}
